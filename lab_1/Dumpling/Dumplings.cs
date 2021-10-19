593e1817c29d033e0d9836d371c7dacb90e05ef8
Фёдор Алексеев <f.alexeeff2010@yandex.ru>
2021-10-19 23:14:01 +0300

    Dumplings реализует интерфейс IEatable

--- lab_1/Dumpling/Dumplings.cs
+++ lab_1/Dumpling/Dumplings.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace lab_1.Dumpling
 {
-    abstract class Dumplings : IEatable
+    abstract class Dumplings: IEatable
     {
         protected static readonly Random rnd = new Random();
 
@@ -135,21 +135,12 @@ namespace lab_1.Dumpling
 
         public virtual string Name()
         {
-            return "класс пельменеобразные ";
+            return "класс пельменеобразное ";
         }
 
         public virtual void Info()
         {
-            Console.WriteLine(Name());
-            Console.WriteLine(exist ? "Слепленные" : "Съеденные");
-            Console.WriteLine(IsCooked ? "Приготовленные" : "Сырые");
-            Console.WriteLine($"Вес: {Weight}г");
-            Console.WriteLine($"% теста: {dough}");
-        }
 
-        public override string ToString()
-        {
-            return "Пельменеобразные";
         }
     }
 }
